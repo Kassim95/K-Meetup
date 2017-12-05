1d9b5e20c9823df961df6bca006aaa4c8f959b80
Kassim Ouattara <ouat.kassim@gmail.com>
2017-12-05 03:20:01 -0500

    populating dynamically the menu

--- src/App.vue
+++ src/App.vue
@@ -5,31 +5,43 @@
       <v-toolbar-title>K-Meetup</v-toolbar-title>
       <v-spacer></v-spacer>
       <v-toolbar-items class="hidden-xs-only">
-        <v-icon left>supervisor_account</v-icon>
-        <v-btn flat>Vew Meetups</v-btn>
+        <v-btn flat v-for="item in menuItems" :key="item.title">
+          <v-icon dark left>{{item.icon}}</v-icon>
+          {{item.title}}
+        </v-btn>
       </v-toolbar-items>
     </v-toolbar>
+
     <v-navigation-drawer v-model="sideNav">
       <v-list>
-        <v-list-tile>
+        <v-list-tile v-for="item in menuItems" :key="item.title">
           <v-list-tile-action>
-            <v-icon>supervisor_account</v-icon>
+            <v-icon>{{item.icon}}</v-icon>
           </v-list-tile-action>
-          <v-list-content>View Meetups</v-list-content>
+          <v-list-tile-content>{{item.title}}</v-list-tile-content>
         </v-list-tile>
       </v-list>
     </v-navigation-drawer>
+    
     <main>
 
     </main>
   </v-app>
 </template>
 
+
 <script>
   export default {
     data () {
       return {
-        sideNav: false
+        sideNav: false,
+        menuItems: [
+          {icon: 'supervisor_account', title: 'Vew Meetups'},
+          {icon: 'room', title: 'Organize Meetup'},
+          {icon: 'person', title: 'Profile'},
+          {icon: 'face', title: 'Sign up'},
+          {icon: 'lock_open', title: 'Sing in'}
+        ]
       }
     }
   }
