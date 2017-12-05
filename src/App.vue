2a219dc926897d8de4d8b30a274f1508518271cc
Kassim Ouattara <ouat.kassim@gmail.com>
2017-12-05 02:28:35 -0500

    Adding toolbar for mobile screen

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,24 @@
 <template>
   <v-app light>
-    <v-navigation-drawer v-model="sideNav">
-
-    </v-navigation-drawer>
     <v-toolbar>
-      <v-toolbar-side-icon @click.native="sideNav = !sideNav"></v-toolbar-side-icon>
+      <v-toolbar-side-icon @click.native="sideNav = !sideNav" class="hidden-sm-and-up"></v-toolbar-side-icon>
       <v-toolbar-title>K-Meetup</v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-toolbar-item>
+      <v-toolbar-items class="hidden-xs-only">
         <v-icon left>supervisor_account</v-icon>
         <v-btn flat>Vew Meetups</v-btn>
-      </v-toolbar-item>
+      </v-toolbar-items>
     </v-toolbar>
+    <v-navigation-drawer v-model="sideNav">
+      <v-list>
+        <v-list-tile>
+          <v-list-tile-action>
+            <v-icon>supervisor_account</v-icon>
+          </v-list-tile-action>
+          <v-list-content>View Meetups</v-list-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
     <main>
 
     </main>
